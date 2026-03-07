a4626ab743d90ebc28296619f118f9b271500837
Shehryar <shehryarf67@gmail.com>
2026-03-07 12:42:55 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+function Box() {
+  return (
+    <mesh>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" metalness={0.4} roughness={0.3} />
+    </mesh>
+  );
+}
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas camera={{ position: [2, 2, 2], fov: 50 }}>
+        <ambientLight intensity={0.6} />
+        <directionalLight position={[5, 5, 5]} intensity={1} />
+        <Box />
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+}

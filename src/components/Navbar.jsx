ee11371707ff486e768516f3053fe69117d8ac3e
Shehryar <shehryarf67@gmail.com>
2026-04-20 20:20:15 +0500

    Adding a menu icon and cross icon for the sidebar to follow up with hamburger menu.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,10 +20,22 @@ const Navbar = () => {
     }
   };
 
+  const handleSidebarToggle = () => {
+    const sidebar = document.querySelector(".sidebar");
+    if (sidebar.style.display === 'flex') {
+      sidebar.style.display = 'none';
+    } else {
+      sidebar.style.display = 'flex';
+    }
+  }
+
   return (
     <header>
       <nav>
-        <ul>
+        <ul className="sidebar">
+          <li onClick={handleSidebarToggle} className="crossburger">
+            <svg xmlns="http://www.w3.org/2000/svg" height="34px" viewBox="0 -960 960 960" width="34px" fill="black"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
+          </li>
           {[
             { label: "Home" },
             { label: "About" },
@@ -34,6 +46,20 @@ const Navbar = () => {
               <a href="#" onClick={(e) => handleNavClick(item.label, e)}>{item.label}</a>
             </li>
           ))}
+        </ul>
+        <ul>
+          {[
+            { label: "Home" },
+            { label: "About" },
+            { label: "Details" },
+            { label: "Contact" }
+          ].map((item) => (
+            <li key={item.label}>
+              <a href="#" onClick={(e) => handleNavClick(item.label, e)}>{item.label}</a>
+            </li>
+          ))}
+          <li onClick={handleSidebarToggle} className="hamburger">
+            <svg xmlns="http://www.w3.org/2000/svg" height="34px" viewBox="0 -960 960 960" width="34px" fill="black"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg> </li>
         </ul>
       </nav>
     </header>

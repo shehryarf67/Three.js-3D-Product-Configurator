c712488a5a4a7c4197c027922be23c4c27eb05c5
Shehryar <shehryarf67@gmail.com>
2026-05-01 22:26:52 +0500

    Importing the correct PNGs into the Hero section and placing them in the proper positions.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,41 @@
 import { useState } from "../imports.js";
 
-import babyBlueFront from "../assets/baby_blue/baby_blue_1.png";
-import babyBlueLeft from "../assets/baby_blue/baby_blue_2.png";
-import babyBlueBack from "../assets/baby_blue/baby_blue_3.png";
-import babyBlueRight from "../assets/baby_blue/baby_blue_4.png";
+import camera1Front from "../assets/camera1/front.png";
+import camera1Rot1 from "../assets/camera1/rot_1.png";
+import camera1Rot2 from "../assets/camera1/rot_2.png";
+import camera1Rot3 from "../assets/camera1/rot_3.png";
+import camera1Rot4 from "../assets/camera1/rot_4.png";
 
-import blossomPinkFront from "../assets/blossom_pink/blossom_pink_1.png";
-import blossomPinkLeft from "../assets/blossom_pink/blossom_pink_2.png";
-import blossomPinkBack from "../assets/blossom_pink/blossom_pink_3.png";
-import blossomPinkRight from "../assets/blossom_pink/blossom_pink_4.png";
+import camera2Front from "../assets/camera2/front.png";
+import camera2Rot1 from "../assets/camera2/rot_1.png";
+import camera2Rot2 from "../assets/camera2/rot_2.png";
+import camera2Rot3 from "../assets/camera2/rot_3.png";
+import camera2Rot4 from "../assets/camera2/rot_4.png";
 
-import lilacPurpleFront from "../assets/lilac_purple/lilac_purple_1.png";
-import lilacPurpleLeft from "../assets/lilac_purple/lilac_purple_2.png";
-import lilacPurpleBack from "../assets/lilac_purple/lilac_purple_3.png";
-import lilacPurpleRight from "../assets/lilac_purple/lilac_purple_4.png";
+import camera3Front from "../assets/camera3/front.png";
+import camera3Rot1 from "../assets/camera3/rot_1.png";
+import camera3Rot2 from "../assets/camera3/rot_2.png";
+import camera3Rot3 from "../assets/camera3/rot_3.png";
+import camera3Rot4 from "../assets/camera3/rot_4.png";
 
-import mintGreenFront from "../assets/mint_green/mint_green_1.png";
-import mintGreenLeft from "../assets/mint_green/mint_green_2.png";
-import mintGreenBack from "../assets/mint_green/mint_green_3.png";
-import mintGreenRight from "../assets/mint_green/mint_green_4.png";
+import camera4Front from "../assets/camera4/front.png";
+import camera4Rot1 from "../assets/camera4/rot_1.png";
+import camera4Rot2 from "../assets/camera4/rot_2.png";
+import camera4Rot3 from "../assets/camera4/rot_3.png";
+import camera4Rot4 from "../assets/camera4/rot_4.png";
+
+import camera5Front from "../assets/camera5/front.png";
+import camera5Rot1 from "../assets/camera5/rot_1.png";
+import camera5Rot2 from "../assets/camera5/rot_2.png";
+import camera5Rot3 from "../assets/camera5/rot_3.png";
+import camera5Rot4 from "../assets/camera5/rot_4.png";
 
 const CAMERA_IMAGES = {
-  "Baby Blue": { front: babyBlueFront, left: babyBlueLeft, back: babyBlueBack, right: babyBlueRight },
-  "Blossom Pink": { front: blossomPinkFront, left: blossomPinkLeft, back: blossomPinkBack, right: blossomPinkRight },
-  "Lilac Purple": { front: lilacPurpleFront, left: lilacPurpleLeft, back: lilacPurpleBack, right: lilacPurpleRight },
-  "Mint Green": { front: mintGreenFront, left: mintGreenLeft, back: mintGreenBack, right: mintGreenRight },
+  "Lilac Purple": { front: camera1Front, left: camera1Rot1, topLeft: camera1Rot2, topRight: camera1Rot3, right: camera1Rot4 },
+  "Clay White": { front: camera2Front, left: camera2Rot1, topLeft: camera2Rot2, topRight: camera2Rot3, right: camera2Rot4 },
+  "Mint Green": { front: camera3Front, left: camera3Rot1, topLeft: camera3Rot2, topRight: camera3Rot3, right: camera3Rot4 },
+  "Blossom Pink": { front: camera4Front, left: camera4Rot1, topLeft: camera4Rot2, topRight: camera4Rot3, right: camera4Rot4 },
+  "Baby Blue": { front: camera5Front, left: camera5Rot1, topLeft: camera5Rot2, topRight: camera5Rot3, right: camera5Rot4 },
 };
 
 const COLORS = Object.keys(CAMERA_IMAGES);
@@ -42,7 +53,8 @@ const Hero = () => {
 
     if (offset === 0) return "center";
     if (offset === 1) return "right";
-    if (offset === 2) return "top";
+    if (offset === 2) return "top-right";
+    if (offset === 3) return "top-left";
     return "left";
   };
 
@@ -50,7 +62,8 @@ const Hero = () => {
     if (position === "center") return CAMERA_IMAGES[color].front;
     if (position === "left") return CAMERA_IMAGES[color].left;
     if (position === "right") return CAMERA_IMAGES[color].right;
-    return CAMERA_IMAGES[color].back;
+    if (position === "top-left") return CAMERA_IMAGES[color].topLeft;
+    return CAMERA_IMAGES[color].topRight;
   };
 
   return (

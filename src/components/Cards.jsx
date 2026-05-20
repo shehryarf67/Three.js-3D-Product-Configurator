bf31f8df48a125e0e612a31d263eda073291186a
Shehryar <shehryarf67@gmail.com>
2026-05-20 18:59:43 +0500

    Setting card colour to teal, changing the heading names, and preventing the animation firing too early

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,21 +5,21 @@ const Cards = () => {
         <div className="card__background"></div>
         <div className="card__content">
           <p className="card__category"> Experience</p>
-          <h3 className="card__heading">Interactive 3D Camera Viewer</h3>
+          <h3 className="card__heading">Films</h3>
         </div>
       </div>
       <div className="card reveal">
         <div className="card__background"></div>
         <div className="card__content">
           <p className="card__category">Customization</p>
-          <h3 className="card__heading">Real-Time Product Controls</h3>
+          <h3 className="card__heading">Cameras</h3>
         </div>
       </div>
       <div className="card reveal">
         <div className="card__background"></div>
         <div className="card__content">
           <p className="card__category">Product Detail</p>
-          <h3 className="card__heading">ACCESSORIES</h3>
+          <h3 className="card__heading">Accessories</h3>
         </div>
       </div>
     </div>
